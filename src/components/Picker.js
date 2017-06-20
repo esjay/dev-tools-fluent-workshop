51232a31b77603770ca34734a5ab939181344d81
esjay <wayne.elgin@gmail.com>
2017-06-20 09:29:03 -0700

    Update readme, clean up some CSS

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -8,7 +8,7 @@ export default class Picker extends Component {
     return (
       <span>
         <h1>{value}</h1>
-        <input onChange={e => onChange(e.target.value)} value={value}/>
+        {/* <input onChange={e => onChange(e.target.value)} value={value}/> */}
         <select onChange={e => onChange(e.target.value)} value={value}>
           {options.map(option => (
             <option value={option} key={option}>
