9f98a862037052eb0ce66c62ef429a6f905f3859
esjay <wayne.elgin@gmail.com>
2017-06-20 09:38:34 -0700

    Use autocomplete for subreddit list

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Autocomplete from 'react-autocomplete';
 
 export default class Picker extends Component {
   render() {
@@ -8,7 +9,22 @@ export default class Picker extends Component {
     return (
       <span>
         <h1>{value}</h1>
-        {/* <input onChange={e => onChange(e.target.value)} value={value}/> */}
+        <Autocomplete
+          getItemValue={(item) => item.label}
+          items={[
+            { label: 'apple' },
+            { label: 'banana' },
+            { label: 'pear' }
+          ]}
+          renderItem={(item, isHighlighted) =>
+            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+              {item.label}
+            </div>
+          }
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          onSelect={(val) => onChange(val)}
+        />
         <select onChange={e => onChange(e.target.value)} value={value}>
           {options.map(option => (
             <option value={option} key={option}>
